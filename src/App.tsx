2f50481981dbd5496615d47cba94e3ce713c4254
Andrew Hyndman <ajhyndman@hotmail.com>
2020-01-17 00:25:10 -0800

    Style the application

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,29 @@ import React, { useEffect, useState } from 'react';
 import { generateCode } from './utils/generateCode';
 import { useTitle } from './utils/useTitle';
 
+const BLUE = '#04A0ED';
+const RED = '#E60C5B';
+const WHITE = 'white';
+
+const BACKGROUND = Math.random() < 0.5 ? RED : BLUE;
+
+const Root: React.FC = ({ children }) => (
+  <div
+    style={{
+      alignItems: 'center',
+      background: BACKGROUND,
+      color: WHITE,
+      display: 'flex',
+      flexDirection: 'column',
+      height: '100vh',
+      justifyContent: 'center',
+      textAlign: 'center',
+    }}
+  >
+    {children}
+  </div>
+);
+
 const App = () => {
   useTitle('Generate a random link code for Pokemon Sword / Shield');
 
@@ -18,7 +41,15 @@ const App = () => {
     return () => document.removeEventListener('keypress', handleRefresh);
   });
 
-  return <div>{code || 'Press Spacebar'}</div>;
+  return (
+    <Root>
+      {code ? (
+        <span style={{ fontSize: 128 }}>{code}</span>
+      ) : (
+        <span style={{ fontSize: 64 }}>Press Spacebar</span>
+      )}
+    </Root>
+  );
 };
 
 export default App;
